0d8dd3f053517d6528c55046084aec32f9a091c8
Jeff Hykin <jeff.hykin@gmail.com>
2026-02-07 17:21:41 -0800

    mark cuda required tests

--- dimos/models/manipulation/contact_graspnet_pytorch/test_contact_graspnet.py
+++ dimos/models/manipulation/contact_graspnet_pytorch/test_contact_graspnet.py
@@ -14,6 +14,7 @@ def is_manipulation_installed() -> bool:
         return False
 
 @pytest.mark.integration
+@pytest.mark.cuda
 @pytest.mark.skipif(not is_manipulation_installed(),
                    reason="This test requires 'pip install .[manipulation]' to be run")
 def test_contact_graspnet_inference() -> None:
